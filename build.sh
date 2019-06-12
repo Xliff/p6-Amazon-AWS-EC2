4fb7147c1294a1d8c57415966eb03a929d2bb176
Xliff <clifton.wood@gmail.com>
2019-06-12 00:38:03 -0400

    - Updated .gitignore - Updated BuildList - Adds build.sh

---
+++ build.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+
+if [ "$1" == "new" ]; then
+  rm LastBuildResults*
+else
+  perl6 scripts/backup_results.pl6
+fi
+echo -e "Dependency Generation\n=====================" >> LastBuildResults
+/usr/bin/time -p -o LastBuildResults -a perl6 scripts/dependencies.pl6
+/usr/bin/time -p /bin/bash -c '(
+  for a in `cat BuildList`; do
+    (
+    	echo " === $a ==="
+	perl6 --stagestats -Ilib -e "use $a" 2>&1
+    )
+  done;
+  echo;
+)' 2>&1 | tee -a LastBuildResults
+unset P6_BUILDING_GTK
